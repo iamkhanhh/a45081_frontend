6f5dddaab0228a5f201a3526a68fce3abce1a0f2
Nguyen Khanh <khanh9102004@gmail.com>
2024-10-26 14:41:21 +0700

    Initial commit

---
+++ enviroment.example.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  appVersion: 'v8.2.4',
+  USERDATA_KEY: 'authf649fc9a5f55',
+  isMockEnabled: true,
+  apiUrl: 'api',
+  appThemeName: 'Metronic',
+  appPurchaseUrl: 'https://1.envato.market/EA4JP',
+  appHTMLIntegration: 'https://preview.keenthemes.com/metronic8/demo2/documentation/base/helpers/flex-layouts.html',
+  appPreviewUrl: 'https://preview.keenthemes.com/metronic8/angular/demo2/',
+  appPreviewAngularUrl: 'https://preview.keenthemes.com/metronic8/angular/demo2',
+  appPreviewDocsUrl: 'https://preview.keenthemes.com/metronic8/angular/docs',
+  appPreviewChangelogUrl: 'https://preview.keenthemes.com/metronic8/angular/docs/changelog',
+};
