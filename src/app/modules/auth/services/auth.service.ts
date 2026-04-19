1d40f087dd72351a935e0d122f44e74788cbd232
iamkhanhh <khanh9102004@gmail.com>
2026-04-19 22:20:49 +0700

    chore: remove userdata key

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -17,7 +17,7 @@ export type UserType = UserModel | undefined;
 export class AuthService implements OnDestroy {
   // private fields
   private unsubscribe: Subscription[] = []; // Read more: => https://brianflove.com/2016/12/11/anguar-2-unsubscribe-observables/
-  private authLocalStorageToken = `${environment.appVersion}-${environment.USERDATA_KEY}`;
+  private authLocalStorageToken = `${environment.appVersion}`;
 
   // public fields
   currentUser$: Observable<UserType>;
